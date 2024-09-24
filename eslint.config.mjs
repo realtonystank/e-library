bac7144d11cbaf93d81c0afa47365ddd0d1652a7
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-09-24 22:52:14 +0530

    better eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+import prettierConfig from "eslint-config-prettier";
 
-export default [
-  { languageOptions: { globals: globals.node } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+const __dirname = dirname(fileURLToPath(import.meta.url));
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  prettierConfig,
+
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+  },
+  {
+    ignores: ["**/node_modules/**", "eslint.config.mjs"],
+  },
+  {
+    rules: {},
+  },
+);
