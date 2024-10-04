4e8ab5b33eacfdcc47e80321dff6a1e5be6ea253
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-10-04 13:25:44 +0530

    add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from "express";
-
+import createHttpError from "http-errors";
+import { globalErrorHandler } from "./common/middlewares/globalErrorHandler";
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req, res, next) => {
+  const err = createHttpError(401, "You are not allowed to access this page");
+  return next(err);
   res.send("Welcome to auth service");
 });
+
+app.use(globalErrorHandler);
 
 export default app;
