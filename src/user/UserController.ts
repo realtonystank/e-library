db2667122a760b1dad9abdb3c2750a777532e5b8
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-10-08 22:47:29 +0530

    create new user

--- src/user/UserController.ts
+++ src/user/UserController.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import createHttpError from "http-errors";
+import { AppDataSource } from "../config/data-source";
+import { User } from "../entity/User";
+import bcrypt from "bcrypt";
+import logger from "../../config/logger";
 
 interface createUser {
   name: string;
@@ -13,7 +17,7 @@ interface createUserRequest extends Request {
 }
 
 export default class UserController {
-  create(req: createUserRequest, res: Response, next: NextFunction) {
+  async create(req: createUserRequest, res: Response, next: NextFunction) {
     const userValidation = validationResult(req);
     if (!userValidation.isEmpty()) {
       console.log(userValidation.array());
@@ -21,6 +25,29 @@ export default class UserController {
       return;
     }
 
-    res.status(201).json({ message: "User registered" });
+    const { name, email, password } = req.body;
+
+    const userRepository = AppDataSource.getRepository(User);
+
+    const userInDb = await userRepository.findOneBy({ email: email });
+
+    if (userInDb) {
+      next(createHttpError(400, "Duplicate email"));
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User();
+    newUser.email = email;
+    newUser.name = name;
+    newUser.password = hashedPassword;
+    const savedUser = await userRepository.save(newUser);
+
+    logger.info(`Created new user with id:${savedUser.id}`);
+
+    res
+      .status(201)
+      .json({ message: `User registered, with id:${savedUser.id}` });
   }
 }
