2768114e8a5c272633f255112a69297e275c9538
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-10-09 23:30:47 +0530

    add refresh and logout routes

--- src/user/UserController.ts
+++ src/user/UserController.ts
@@ -112,4 +112,60 @@ export default class UserController {
 
     res.status(200).json({ ...userInDb, password: "" });
   }
+
+  async refresh(req: AuthRequest, res: Response, next: NextFunction) {
+    const userId = req.auth.sub;
+    const refreshTokenRepository = AppDataSource.getRepository(Refresh_Tokens);
+    const userRepository = AppDataSource.getRepository(User);
+
+    const userInDb = await userRepository.findOneBy({ id: Number(userId) });
+
+    if (!userInDb) {
+      next(createHttpError(400, "Could not find the user with the token"));
+      return;
+    }
+
+    const newAccessToken = sign({ sub: userId }, Config.ACCESS_TOKEN_SECRET, {
+      expiresIn: "1h",
+    });
+    const newRefreshToken = sign({ sub: userId }, Config.REFRESH_TOKEN_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.cookie("AccessToken", newAccessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60,
+    });
+
+    res.cookie("RefreshToken", newRefreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    await refreshTokenRepository.update(
+      { user: userInDb },
+      { token: newRefreshToken },
+    );
+
+    res.status(200).json({ message: "success" });
+  }
+
+  async logout(req: AuthRequest, res: Response) {
+    const { RefreshToken } = req.cookies as {
+      AccessToken: string;
+      RefreshToken: string;
+    };
+
+    res.clearCookie("AccessToken");
+    res.clearCookie("RefreshToken");
+
+    const refreshTokenRepository = AppDataSource.getRepository(Refresh_Tokens);
+    await refreshTokenRepository.delete({ token: RefreshToken });
+
+    res.status(200).json({ message: "success" });
+  }
 }
--- src/user/userRouter.ts
+++ src/user/userRouter.ts
@@ -10,6 +10,7 @@ import UserController from "./UserController";
 import { asyncWrapper } from "../common/utils/wrapper";
 import authenticate from "../common/middlewares/authenticate";
 import { AuthRequest } from "../types";
+import validateRefreshToken from "../common/middlewares/validateRefreshToken";
 
 const userRouter = express.Router();
 const userController = new UserController();
@@ -35,6 +36,22 @@ userRouter.post(
   authenticate as RequestHandler,
   asyncWrapper((req: Request, res: Response) =>
     userController.self(req as AuthRequest, res),
+  ),
+);
+
+userRouter.post(
+  "/refresh",
+  validateRefreshToken as RequestHandler,
+  asyncWrapper((req: Request, res: Response, next: NextFunction) =>
+    userController.refresh(req as AuthRequest, res, next),
+  ),
+);
+
+userRouter.post(
+  "/logout",
+  authenticate as RequestHandler,
+  asyncWrapper((req: Request, res: Response) =>
+    userController.logout(req as AuthRequest, res),
   ),
 );
 
