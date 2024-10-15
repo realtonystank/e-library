3bf30a39964e1fe5501b318ad9c262e9bf182b39
Priyansh Rajwar <rajwars.priyansh@gmail.com>
2024-10-16 00:22:29 +0530

    upload book to cloudinary

--- src/common/services/CloudinaryStorage.ts
+++ src/common/services/CloudinaryStorage.ts
@@ -1,6 +1,7 @@
-import { FileData, FileStorage } from "../../types/storage";
+import { FileData, FileStorage, UploadSuccess } from "../../types/storage";
 import { v2 as cloudinary, ConfigOptions } from "cloudinary";
 import { Config } from "../../config/config";
+import path from "node:path";
 
 export class CloudinaryStorage implements FileStorage {
   private client: ConfigOptions;
@@ -12,8 +13,45 @@ export class CloudinaryStorage implements FileStorage {
     });
   }
 
-  upload(fileData: FileData): void {
-    console.log(fileData);
+  async upload(fileData: FileData): Promise<UploadSuccess> {
+    const files = fileData.files as {
+      [fieldname: string]: Express.Multer.File[];
+    };
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const fileName = files.coverImage[0].filename;
+    const filePath = path.resolve(
+      __dirname,
+      "../../../public/data/uploads",
+      fileName,
+    );
+
+    const uploadResult = await cloudinary.uploader.upload(filePath, {
+      filename_override: fileName,
+      folder: "book-covers",
+      format: coverImageMimeType,
+    });
+
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../../public/data/uploads",
+      bookFileName,
+    );
+
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-file",
+        format: "pdf",
+      },
+    );
+
+    return {
+      coverImageUrl: uploadResult.secure_url,
+      bookFileUrl: bookFileUploadResult.secure_url,
+    };
   }
   delete(fileName: string): void {
     console.log(fileName);
